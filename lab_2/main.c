348ce368531fab0e9d1af5d91f56730ac39cf2e5
dejan <dejan@meznarc.si>
2023-03-12 11:39:08 +0100

    Made lab2 work

--- lab_2/main.c
+++ lab_2/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <iso646.h>
-#include "math.h"
+#include <math.h>
 
 #define ACCURACY 0.00001
 
@@ -12,14 +12,9 @@ float guessSqrtCloser(float number, float guess);
 int main() {
     printf("Sqrt of 2= %f (expected %f)\n\n", guessSqrtCloser(2,84), sqrtf(2));
 
-    int x = 4;
-    int y = 3;
-    x=20 || y++ ;
-    printf("%d",x);
 
     printf("Trying for all: \ncalcluated\treal\n");
     for (int i = 0; i < 30; ++i) {
-
         float clac = guessSqrtCloser((float)i,2);
 
         printf("%f\t%f\n", clac, sqrt(i));
