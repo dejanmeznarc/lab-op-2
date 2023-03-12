46c0ea443bd5e14b0ad1d231dd81917e731f68ed
dejan <dejan@meznarc.si>
2023-03-12 12:20:13 +0100

    works

--- lab_3/main.c
+++ lab_3/main.c
@@ -7,23 +7,52 @@
 
 complex double guessThirdRootCloser(complex double number, complex double guess);
 
+void printComplex(complex double num);
+
+int isInRangeComplex(complex double guess, complex double solution) {
+
+    complex double delata = cabs(guess - solution);
+
+    return (creal(delata) < ACCURACY) &&
+           (cimag(delata) < ACCURACY);
+
+}
+
+#define SOLUTIONS_NUM 3
+const complex double solutions[SOLUTIONS_NUM] = {
+        1,
+        -0.5 + 0.866025404 * I, // -0.5 + I*sqrt(3)/2
+        -0.5 - 0.866025404 * I, // -0.5 + I*sqrt(3)/2
+};
+
 
 int main() {
 
 
-    complex double solution = guessThirdRootCloser(1, 0.8 * I);
+    complex double solution = guessThirdRootCloser(1, -1+4*I);
 
-    printf("Complex: Re= %f Im= %f\n", creal(solution), cimag(solution));
+    printComplex(solution);
+
     return 0;
 }
 
 
 complex double guessThirdRootCloser(complex double number, complex double guess) {
+
     guess = guess - (guess * guess * guess - number) / (3 * guess * guess);
 
-
-    if (cabs(guess - sqrt(number)) > ACCURACY) {
-        return guessThirdRootCloser(number, guess);
+    for (int i = 0; i < SOLUTIONS_NUM; ++i) {
+        if (isInRangeComplex(guess, solutions[i])) {
+            return guess;
+        }
     }
-    return guess;
+
+    return guessThirdRootCloser(number, guess);
 }
+
+void printComplex(complex double num) {
+    printf("Complex: Re= %f Im= %f\n", creal(num), cimag(num));
+}
+
+
+
